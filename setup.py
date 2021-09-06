e762ce64a6ccf96ba67bf69fffa0c843f3dea1d6
Joel Herrera Vázquez <joel@astro.unam.mx>
2021-09-06 14:09:11 -0700

    casi

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Kraken-Optical-Simulator',
+    version='0.1',
+    packages=['KrakenOS', 'KrakenOS.AstroAtmosphere'],
+    url='https://github.com/Garchupiter/Kraken-Optical-Simulator',
+    license='GNU General Public License v3.0',
+    author='joel Herrera et al.',
+    author_email='joel@astro.unam.mx',
+    description='Optical Simulation and ray tracing '
+)
