eafa80a7cc1c2c41c029da883b79ae163f0640c8
Joel Herrera Vázquez <joel@astro.unam.mx>
2021-09-11 08:44:06 -0700

    v1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='KrakenOS',
-    version='0.0.32',
+    version='1.0',
     packages=['KrakenOS', 'KrakenOS.AstroAtmosphere', 'KrakenOS.Cat', 'KrakenOS.Examples', 'KrakenOS.Docs'],
 
     install_requires=['pyvista','PyVTK','vtk','numpy','scipy','matplotlib', 'csv342'],
